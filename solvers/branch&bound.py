53cda09005179c256442c4b7887257b85820c620
Matrixeigs <matrixeigs@gmail.com>
2018-04-25 10:17:22 +0800

    Update solvers in the optimization packages

--- solvers/branch&bound.py
+++ solvers/branch&bound.py
@@ -4,4 +4,5 @@ Branch and bound method for mixed-integer linear programming problems
 @author: Zhao Tianyang
 @e-mail:zhaoty@ntu.edu.sg
 This algorithm is developed as a test algorithm for solving mixed-integer linear programming problems.
+This algorithm is based on the interior point methods, under the same documentary.
 """
