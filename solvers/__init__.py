822b735a0fa27af674593266bc39e1a34db11872
Matrixeigs <matrixeigs@gmail.com>
2018-04-25 10:34:15 +0800

    Update solvers design

---
+++ solvers/__init__.py
@@ -0,0 +1,21 @@
+"""
+Solvers for the optimization package
+The following problems can be solved.
+1) Linear programming (LP)
+2) Quadratic programming (QP)
+3) Linear conic programming (LCP)
+4) Mixed integer linear programming (MILP)
+5) Mixed integer quadratic programming (MIQP)
+6) Quadratic constrained quadratic programming (QCQP), convex and non-convex
+
+The following packages can ba adopted in place of these packages.
+1) CVXOPT
+2) Mosek
+3) Gurobi
+4)
+
+
+
+@author: Zhao Tianyang
+@e-mail: zhaoty@ntu.edu.sg
+"""
