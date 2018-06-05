101c15693da38063be313e8aa2c8e08169bf99ce
Matrixeigs <matrixeigs@gmail.com>
2018-06-05 17:08:26 +0800

    Update data format

--- unit_commitment/wind_hydro/interval_unit_commitment.py
+++ unit_commitment/wind_hydro/interval_unit_commitment.py
@@ -3,7 +3,11 @@ Interval unit commitment
 @author:Zhao Tianyang
 @e-mail:zhaoty@ntu.edu.sg
 """
-
+from numpy import zeros, shape, ones, diag, concatenate, r_, arange, divide
+import matplotlib.pyplot as plt
+from solvers.mixed_integer_quadratic_programming import mixed_integer_quadratic_programming as miqp
+import scipy.linalg as linalg
+from scipy.sparse import csr_matrix as sparse
 
 class IntervalUnitCommitment():
     ""
@@ -11,7 +15,7 @@ class IntervalUnitCommitment():
     def __init__(self):
         self.name = "Interval Unit Commitment"
 
-    def problem_formulation(self):
+    def problem_formulation(self, case, Load_profile):
         # Hydro power unit
         IG = 0
         PG = 1
@@ -23,3 +27,4 @@ class IntervalUnitCommitment():
         PLC = 6
         # Output of wind power unit
         PWG = 7
+
