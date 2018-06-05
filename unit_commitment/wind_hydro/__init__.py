c9c4761fc137d3753a6febdb99ece55a13905f13
Matrixeigs <matrixeigs@gmail.com>
2018-06-05 15:08:51 +0800

    Modify the test cases

--- unit_commitment/wind_hydro/__init__.py
+++ unit_commitment/wind_hydro/__init__.py
@@ -2,4 +2,10 @@
 Jointed wind and hydro power dispatch under virtual power plan framework
 @author: Zhao Tianyang
 @e-mail: zhaoty@ntu.edu.sg
+# test the
 """
+
+from unit_commitment.test_cases.case14 import case14
+from pypower import runopf
+
+sol = runopf.runopf(case14())
