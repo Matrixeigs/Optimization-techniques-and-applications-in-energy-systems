994f69db7a006a40073a73fdee3b7c4f3e961b37
Matrixeigs <matrixeigs@gmail.com>
2018-06-09 23:34:15 +0800

    Update standard models

---
+++ solvers/two_stage_robust_optimization.py
@@ -0,0 +1,28 @@
+"""
+Solvers for two-stage robust optimization problems
+References:
+    [1]Solving two-stage robust optimization problems using a column-and-constraint generation method
+    The standard format is equation (1)
+
+"""
+
+
+
+class TwoStageRobustOptimization():
+    """
+    column-and-constraint generation method for two-stage robust optimization
+    one kind of primal-dual cutus methods
+    """
+
+    def __init__(self):
+        self.name = " Robust optimization"
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    robust_optimization = TwoStageRobustOptimization()
