e442cdfafd898562beedb6275dc6b15e29cff1ba
Matrixeigs <matrixeigs@gmail.com>
2018-06-14 20:04:07 +0800

    Add two packages

---
+++ transportation_systems/__init__.py
@@ -0,0 +1,8 @@
+"""
+Transportation system electrification
+The following models are introduced for the optimization of traffic-power networks
+1) Time space networks
+2) Transportable energy storage systems
+@author:Zhao Tianyang
+@e-mail:zhaoty@ntu.edu.sg
+"""
