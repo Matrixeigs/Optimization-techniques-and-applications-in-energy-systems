439c70abebc5f69c49008f0ab0975bb794771f94
Matrixeigs <matrixeigs@gmail.com>
2018-07-04 21:50:55 +0800

    add hybrid AC DC network modelling class

--- distribution_system_optimization/hybrid_AC_DC_distribution_networks.py
+++ distribution_system_optimization/hybrid_AC_DC_distribution_networks.py
@@ -1,3 +1,5 @@
 """
 Hybrid AC/DC distribution network modelling
+This class is to formulate a genetic network modelling technique for hybrid AC/DC networks
+Novel data formats should be introduced to represent the connection between the AC bus and DC bus
 """
