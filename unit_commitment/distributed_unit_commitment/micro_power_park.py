506a3b38117c0965f7d7b4cd0c9a59d543bc86ac
Matrixeigs <matrixeigs@gmail.com>
2018-07-22 10:32:45 +0800

    distributed unit commitment package

---
+++ unit_commitment/distributed_unit_commitment/micro_power_park.py
@@ -0,0 +1,12 @@
+"""
+Distributed unit commitment for micro-gird power park
+"""
+
+
+class UnitCommitmentPowerPark():
+    """
+    Unit commitment for power park
+    """
+
+    def __init__(self):
+        self.name = "Power park"
