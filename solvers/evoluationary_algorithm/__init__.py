64dd92099b2f02526c4547f4c34d32d59756423e
Matrixeigs <matrixeigs@gmail.com>
2018-08-15 10:14:39 +0800

    add init for EAs

--- solvers/evoluationary_algorithm/__init__.py
+++ solvers/evoluationary_algorithm/__init__.py
@@ -1,2 +1,10 @@
-###
-###
+# The evoluationary algorithm package
+# The following algorithms are included:
+# 1) Genetic algorithm
+# 2) Differential evoluation
+# 3) PSO
+# 4) Hybrid searching algorithm
+
+# The following frameworks are provided
+# 1) Unconstrained/Constrained optimization
+# 2) Single/multi-objective optimization
